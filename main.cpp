504babfd46a5d4b2769cf0a97616a2d018553c6d
José Nascimento <joseaugustodearaujonascimento@gmail.com>
2018-02-01 12:31:41 -0300

    Add filename argument support

--- main.cpp
+++ main.cpp
@@ -1,11 +1,21 @@
 #include "kaleidoscope.yy.hpp"
 #include "driver.hpp"
 
-int main()
+int main(int argc, char **argv)
 {
-  driver context;
-  yy::parser parser(context);
+  driver* context = nullptr;
+
+  if (argc > 1)
+    context = new driver(argv[1]);
+  else
+    context = new driver();
+
+  yy::parser parser(*context);
+  context->scan_begin();
   int result = parser.parse();
+  context->scan_end();
+
+  delete context;
 
   return result;
 }
